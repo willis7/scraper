22d00e53422e16e42ea5957a2f9b82e550390a2e
willis7 <sion5@hotmail.co.uk>
2015-01-12 22:44:17 +0000

    Really simple scraper using jsoup on a wikipedia page.

---
+++ src/main/java/com/willis/scraper/Scraper.java
@@ -0,0 +1,59 @@
+package com.willis.scraper;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import java.net.*;
+import java.io.*;
+
+/**
+ * Created by willis7 on 12/01/15.
+ */
+public class Scraper {
+    public static void main(String[] args) {
+        scrapeTopic("/wiki/python");
+    }
+
+    /**
+     * Finds all the elements that match the select string, selects the first
+     * element of this set, and turns the resulting object into plain text.
+     *
+     * @param url
+     */
+    private static void scrapeTopic(String url) {
+        String html = getUrl("http://www.wikipedia.org/"+url);
+        Document doc = Jsoup.parse(html);
+        String contentText = doc.select("#mw-content-text > p").first().text();
+        System.out.println(contentText);
+    }
+
+    /**
+     * Takes in an arbitrary ULR and returns the raw source code as a string.
+     * @param url
+     * @return the output from the BufferedReader object
+     */
+    private static String getUrl(String url) {
+        URL urlObj = null;
+        try {
+            urlObj = new URL(url);
+        }catch(MalformedURLException e) {
+            System.out.println("The URL was malformed");
+            return "";
+        }
+
+        URLConnection urlCon = null;
+        BufferedReader in = null;
+        String outputText = "";
+        try {
+            urlCon = urlObj.openConnection();
+            in = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+            String line = "";
+            while((line = in.readLine()) != null) {
+                outputText += line;
+            }
+        }catch(IOException e) {
+            System.out.println("There was an error connecting to the URL");
+            return "";
+        }
+        return outputText;
+    }
+}
